0095b573ae7faeceb9f4a59773464f5f7f715fd8
jarabt <45106556+jarabt@users.noreply.github.com>
2020-01-21 19:59:50 +0100

    Add files via upload

---
+++ prvni.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/*
+int calculateTriangularNumber (int n)
+{
+	int i, triangularNumber = 0;
+	for ( i =1; i <= n; ++i )
+	triangularNumber += i;
+	return triangularNumber;
+}
+
+int main (void)
+{
+	printf ("Triangular number %i is %i\n", 10, calculateTriangularNumber (10));
+	printf ("Triangular number %i is %i\n", 20, calculateTriangularNumber (20));
+	printf ("Triangular number %i is %i\n", 30, calculateTriangularNumber (50));	
+	return 0;
+}
+*/
+
+int compute (char src[], char result[], int n)
+{
+	int intValue1, intValue2, result = 0;
+
+	int size_of_src = sizeof(src);
+	for (int i = 0;  i < (size_of_src - 1); i++)
+	{
+
+
+
+int main (void)
+{
+	char r[5];
+	int v;
+	v = compute("10 20", r, 5);
+	if (v)
+		printf(%s\n", r);
+	else
+		print("vysledek je moc velky\n");
+
+	return 0;
+}
+
+
+
+
